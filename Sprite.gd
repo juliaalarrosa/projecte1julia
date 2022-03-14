75722f0f27206da6ef2fbb52d5b8d39da527e5ea
juliaalarrosa <101257298+juliaalarrosa@users.noreply.github.com>
2022-03-14 14:28:49 +0100

    area i moviment
    el pare es mou amb la seva area 2d

--- Sprite.gd
+++ Sprite.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends Area2D
 
 
 # Declare member variables here. Examples:
@@ -7,6 +7,7 @@ extends Sprite
 #var velocitat = Vector2(200,100)
 export var velocitat = Vector2(200,300)
 var velocitat_angular = 30
+var direccio = Vector2(0,0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -16,12 +17,26 @@ func _ready():
 #func _process(delta):
 #	pass
 func _process(delta):
-	position+= velocitat*delta
-	rotation_degrees += 30 *delta
+	direccio = Vector2.ZERO
+	if Input.is_action_pressed("dreta"):
+		direccio += Vector2(1,0)
+	if Input.is_action_pressed("esquerra"):
+		direccio += Vector2(-1,0)
+	if Input.is_action_pressed("devant"):
+		direccio += Vector2(0,-1)
+	if Input.is_action_pressed("derrera"):
+		direccio += Vector2(0,1)	
 	
-	if position.x >= 1024 or position.x <= 0:
-		velocitat.x = velocitat.x * (-1)
-	if position.y >= 600 or position.y <= 0:
-		velocitat *= -1
 	
-	$fill.rotation_degrees += velocitat_angular
+	
+	
+	position += direccio.normalized() * velocitat * delta
+	#position+= velocitat*delta
+	#rotation_degrees += 30 *delta
+	
+	#if position.x >= 1024 or position.x <= 0:
+	#	velocitat.x = velocitat.x * (-1)
+	#if position.y >= 600 or position.y <= 0:
+	#	velocitat *= -1
+	
+	#$fill.rotation_degrees += velocitat_angular
